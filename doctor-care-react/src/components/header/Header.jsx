f05d3906728be7ff44c2172bc7b8c786cd9231ac
ELIEL DOUNA <elielreich@yahoo.com>
2022-08-26 00:01:15 -0300

    update

--- doctor-care-react/src/components/header/Header.jsx
+++ doctor-care-react/src/components/header/Header.jsx
@@ -5,10 +5,55 @@ import Navbar from '../navbar/Navbar'
 const Header = () => {
   return (
     <>
-    <Navbar/>
-    <div className="header">
-
-    </div>
+      <Navbar />
+      <div className="header">
+        <div className="left">
+          <div className="welcome">
+            BOAS-VINDAS A DOCTORCARE 👋
+          </div>
+          <div className="assitencia">
+            Assistência médica <br /> simplificada para todos
+          </div>
+          <div className="txt">
+            Os médicos da DoctorCare vão além dos sintomas para tratar a causa <br /> raiz de sua doença e proporcionar uma cura a longo prazo.
+          </div>
+          <div className="btn">
+            <div className="btn-icon">
+              
+            </div>
+            <div className="btn-txt">
+              Agende sua consulta
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          {/* <img src="../../assets/4.png " alt="" /> */}
+          <div className="right-shadow"></div>
+        </div>
+        <div className="counter">
+          <div className="sec1">
+            <div className="txt1">
+            +3.500
+            </div>
+            <div className="txt2">
+            Pacientes atendidos
+            </div>
+          </div>
+          <div className="line1"></div>
+          <div className="sec2">
+            <div className="txt3">
+            +15
+            </div>
+            <div className="txt4">
+            Especialistas disponíveis
+            </div>
+          </div>
+          <div className="sec2">
+            <div className="txt5"></div>
+            <div className="txt6"></div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
