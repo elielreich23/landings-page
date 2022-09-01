d39637521fdc3d47e381db0f38a92465c30f1627
ELIEL DOUNA <elielreich@yahoo.com>
2022-09-01 15:17:38 -0300

    update

--- doctor-care-react/src/App.jsx
+++ doctor-care-react/src/App.jsx
@@ -1,4 +1,5 @@
 //falta crea a reponsividade para cada components
+//quando tu vai editar o scss tu pode commentar meu codigo e refazer o estilo se dar muito trabalho so editar o sass do jeito que eu fiz
 import React from 'react'
 import Header from './components/header/Index'
 import Servicos from './components/servicos/Index'
