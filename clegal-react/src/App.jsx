ecc65492a7a5fcd01cea07ff8f35263dbada6df2
ELIEL DOUNA <elielreich@yahoo.com>
2022-09-13 16:46:42 -0300

    commit

--- clegal-react/src/App.jsx
+++ clegal-react/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import  './App.scss'
+import Header from './components/header/Index'
 
 const App = () => {
   return (
     <>
     <div className="App">
+      <Header/>
       
     </div>
     
---
+++ clegal-react/src/components/header/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.scss'
+const Header = () => {
+  return (
+    <>
+        <div className="header">
+          jjdk
+        </div>
+
+    </>
+  )
+}
+
+export default Header
